409517fda7d0adeaba8a1f35bd3df2c6c79acd28
jxmeslukin <jimmy.lukin@gmail.com>
2021-02-02 22:08:20 +1100

    v1

---
+++ JSTetris.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () =>{
+//all code in here
+
+const grid = document.querySelector('.grid');
+let squares = Array.from(document.querySelectorAll('.grid div'));
+const width = 10
+const scoreDisplay = document.querySelector('#score');
+const startBtn = document.querySelector('#start-button');
+
+console.log(squares);
+
+})
